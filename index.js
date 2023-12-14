a443c5018639e097d57e38463ba24597c29f1b17
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-14 15:23:35 +0600

    wip- image uploaded

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import exphbs from "express-handlebars";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { upload } from "./uploader/uploader.js";
 
 import uploadRouter from "./controllers/uploadController.js";
 import userRoute from "./routes/userRoute.js";
@@ -30,6 +31,25 @@ app.set("view engine", ".hbs");
 app.use('/images',express.static('public/images'))
 app.use('/',userRoute);
 app.use('/api/upload',uploadRouter);
+
+const logoUpload = upload.single("image");
+app.post("/test/upload", (req, res) => {
+  logoUpload(req, res, (err) => {
+    if (err) {
+      // console.log(err);
+      if (err.code === "LIMIT_FILE_SIZE") {
+        return res.status(400).send("file size exceeds maximum limit");
+      }
+      console.log(err.message);
+      return res.status(400).send({ error: err.message });
+    }
+    //when no error
+    // console.log("save the file", req.file);
+    // console.log(`here's the file path:::`, req.file.path);
+    console.log("i am here::", req.file.path);
+    return res.send("file uploaded successfully");
+  });
+});
 
 
 app.listen(port, () => {
---
+++ uploader/uploader.js
@@ -0,0 +1,62 @@
+import multer from "multer";
+import path from "path";
+
+const maxSize = 1024 * 1024 * 10;
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const date = Date.now();
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    const extensionName = path.extname(file.originalname);
+    // console.log(extensionName);//comes with . i.e .jpg
+    cb(null, file.fieldname + "-" + uniqueSuffix + extensionName);
+  },
+});
+
+export const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    // const validMimeTypes = "image";
+    const ext = path.extname(file.originalname);
+    const mime = file.mimetype;
+    // console.log("this is mime:::", mime);
+
+    if (
+      mime !== "image" &&
+      ext !== ".png" &&
+      ext !== ".jpg" &&
+      ext !== ".gif" &&
+      ext !== ".jpeg"
+    ) {
+      // console.log("i am here");
+      return callback(new Error("Only images allowed"));
+    }
+    callback(null, true);
+  },
+  limits: { fileSize: maxSize },
+});
+// export const upload = multer({
+//   storage: storage,
+//   fileFilter: (req, file, callback) => {
+//     // const validMimeTypes = "image";
+//     const ext = path.extname(file.originalname);
+//     const mime = file.mimetype;
+//     // console.log("this is mime:::", mime);
+
+//     if (
+//       mime !== "image" &&
+//       ext !== ".png" &&
+//       ext !== ".jpg" &&
+//       ext !== ".gif" &&
+//       ext !== ".jpeg"
+//     ) {
+//       return callback(new Error("Only images allowed"));
+//       // throw Error;
+//     }
+//     callback(null, true);
+//   },
+//   limits: { fileSize: maxSize },
+// });
