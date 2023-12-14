9b242470844efd4c31f0056bc6fe166b95959c7a
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-14 10:41:07 +0600

    wip upload image

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import exphbs from "express-handlebars";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import uploadRouter from "./controllers/uploadController.js";
 import userRoute from "./routes/userRoute.js";
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static("public"));
 
